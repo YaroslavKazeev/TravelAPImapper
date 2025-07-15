c346750d4966ee599dcd95bcd6ef517576d2319a
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-07-15 18:12:13 +0200

    Cleaned, suggestions work, initMap error still stays

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -4,28 +4,31 @@ import { fetchSuggestions } from "../util/fetchSuggestions.js";
 
 export function createMainPage() {
   createMainView();
-}
+  try {
+    const sessionToken = new google.maps.places.AutocompleteSessionToken();
 
-const sessionToken = new google.maps.places.AutocompleteSessionToken();
-
-let debounceTimeout;
-function inputEventListeners(input) {
-  input.addEventListener("input", () => {
-    clearTimeout(debounceTimeout);
-    if (input.value.trim()) {
-      debounceTimeout = setTimeout(async () => {
-        try {
-          await fetchSuggestions(input.value.trim(), sessionToken);
-        } catch (error) {
-          console.log(error);
-          suggestionErrorView(error);
+    let debounceTimeout;
+    function inputEventListener(input) {
+      input.addEventListener("input", () => {
+        clearTimeout(debounceTimeout);
+        if (input.value.trim()) {
+          debounceTimeout = setTimeout(async () => {
+            try {
+              await fetchSuggestions(input.value.trim(), sessionToken);
+            } catch (error) {
+              suggestionErrorView(error);
+            }
+          }, 500);
         }
-      }, 500);
+      });
     }
-  });
-}
 
-const inputStart = document.getElementById("startLoc");
-const inputEnd = document.getElementById("endLoc");
-inputEventListeners(inputStart);
-inputEventListeners(inputEnd);
+    const inputStart = document.getElementById("startLoc");
+    const inputEnd = document.getElementById("endLoc");
+    inputEventListener(inputStart);
+    inputEventListener(inputEnd);
+  } catch (error) {
+    console.log(error);
+    suggestionErrorView(error);
+  }
+}
